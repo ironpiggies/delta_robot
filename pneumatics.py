5805d5a6fcb805ddab6ed09723f5eb9b049e56e2
Nicolas Arons <nicarons@gmail.com>
2019-05-02 16:29:49 -0400

    got it to work! serial connection with arduino successful

--- pneumatics.py
+++ pneumatics.py
@@ -19,21 +19,10 @@ def send_pump_cmd(pump_cmd,serialComm):
     try:
         str_cmd =  str(pump_cmd) + '\n'
         serialComm.write(str_cmd) #send pump value to arduino
-        return True
+        return True #return true if it works
     except:
         print("Can't send pump command")
-        return False
-
-def get_pump_state(serialComm):
-    try:
-        serialData = serialComm.readline()
-        if serialData:
-            return True
-        else:
-            return False
-    except:
-        print("Didn't read from Arduino")
-        return False
+        return False #return false if it doesnt work
 
 #below is for testing
 #ser=start_serial("/dev/cu.usbmodem1421")
