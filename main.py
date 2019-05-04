04052f38bc3ea0f8e5702afd260f37ff7f7cd210
Nicolas Arons <nicarons@gmail.com>
2019-05-04 00:01:19 -0400

    fleshed out a bit more of the functions in pseudocode/comments

--- main.py
+++ main.py
@@ -26,8 +26,20 @@ def put_toppings_on():
     also do salt and pepper shaker?
     '''
     print("Starting to put toppings on!")
-
-
+    #while True:
+    #move arms out of the way (can this even be done?)
+    #capture image
+    #toppings=color_code.get_all_toppings(image)
+    #pizza=color_code.get_pizza(image) #gives pizza location and holes to place toppings into
+    #chosen_hole=hole_selector(toppings,pizza)#check to make sure no toppings in the hole we want to place in
+    #top=topping_selector(toppings,pizza) #select based on criteria so that we don't choose toppings that are too close to pizza or other toppings
+    #path_to_topping=get_path(current_loc,top) #gives path from current position to where we want to do topping things
+    #path_to_drop=get_path(top,chosen_hole)
+    #move_along_path(path_to_topping) #only move on when we have the go ahead form the odrives
+    #start_pickup_sequence() #this involves changing z position, blowing up baloon, force control to lower on topping and suction to grab it, then raising back up (keeping suction)
+    #move_along_path(path_to_drop)
+    #start_drop_sequence() #should just be releasing suction and/or blowing out
+    #end while loop :P
     return
 
 def deliver_pizza():
@@ -36,7 +48,7 @@ def deliver_pizza():
     to tell us that it is ready. then we execute command to push pizza. then tell
     mobile that we've pushed it off
     '''
-    print("Starting to deliver_pizza!")
+    print("Starting to deliver pizza!")
 
     #send_pizza_ready_signal() or something
     #while !mobile_in_position():
@@ -53,6 +65,9 @@ def make_dough():
     will require pneumatics to be involved as well probably :P
     '''
     print("Starting to make dough!")
-
+    #while !find_dough():
+    #   continue
+    #(x,y)=find_dough()
+    #mash()
 
     return
